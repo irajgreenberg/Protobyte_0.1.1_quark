638e6157084cee5d0e294ea4339b22834e8137cf
irajgreenberg <irajgreenberg@gmail.com>
2015-02-02 07:29:31 -0600

    more mods to immediate rendering system

--- Protobyte/src/myApp01.cpp
+++ Protobyte/src/myApp01.cpp
@@ -14,25 +14,25 @@ void myApp01::init() {
 	globalAmbient = Col3f(.65, .65, .65);
 
 	//light0.setPosition(Vec3f(.1, 300, 0));
-	light0.setPosition(Vec3f(0, 150, 0));
+	light0.setPosition(Vec3f(0, 150, -50));
 	light0.setIntensity(Vec3f(.85, .85, .85));
 
-	/*light1.setPosition(Vec3f(-10, -400, 0));
-	light1.setIntensity(Vec3f(1, 1, 1));*/
+	light1.setPosition(Vec3f(-10, -400, 0));
+	light1.setIntensity(Vec3f(1, 1, 1));
 	//camera0.setPosition(Vec3f(0, 0, 500));
 
 
 	shadowsOn();
 	// wall
-	plane = GroundPlane(Vec3(), Vec3(), Dim2f(200, 200), Col4f(1, 1, 1, 1), 1, 1, "corroded_red.jpg", Vec2f(1, 1));
+	plane = GroundPlane(Vec3(), Vec3(), Dim2f(50, 50), Col4f(1, 1, 1, 1), 1, 1, "yellow2.jpg", Vec2f(1, 1));
 	//GroundPlane()
 	plane.textureOn();
-	plane.setBumpMap("corroded_red.jpg");
-	plane.loadBumpMapTexture("corroded_red.jpg");
-	plane.setTextureScale(Vec2f(.5));
+	plane.setBumpMap("yellow2.jpg");
+	plane.loadBumpMapTexture("yellow2.jpg");
+	plane.setTextureScale(Vec2f(.05));
 	plane.setAmbientMaterial(Col4f(.3, .3, .3, 1.0));
-	plane.setSpecularMaterial(Col4f(.85, .85, .85, 1.0));
-	plane.setShininess(15);
+	plane.setSpecularMaterial(Col4f(1, 1, 1, 1.0));
+	plane.setShininess(6);
 	//trace("GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS =", GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS);
 
 
@@ -214,8 +214,8 @@ void myApp01::display() {
 	////pop();
 
 	push();
-	translate(0, -600, 0);
-	scale(50, 1, 50);
+	translate(0, -100, 0);
+	scale(20, 1, 20);
 	plane.display();
 	pop();
 	//rotate(getFrameCount()*.05f, 1, 0, 0);
