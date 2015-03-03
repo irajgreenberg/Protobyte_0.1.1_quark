6cbd2695a5b61ecea34c3ab5dd207ae307ef3a00
irajgreenberg <irajgreenberg@gmail.com>
2015-03-02 23:12:32 -0600

    added more collisionTest files

---
+++ Protobyte/src/CollisionTest03.cpp
@@ -0,0 +1,11 @@
+
+#include "CollisionTest03.h"
+
+void CollisionTest03::init() {
+}
+
+void CollisionTest03::run() {
+}
+
+void CollisionTest03::display() {
+}
---
+++ Protobyte/src/CollisionTest03.h
@@ -0,0 +1,20 @@
+
+#ifndef __COLLISIONTEST03_H__
+#define __COLLISIONTEST03_H__
+
+#include <iostream>
+#include "appProtobyte/ProtoBaseApp.h"
+
+using namespace ijg;
+
+class CollisionTest03 : public ProtoBaseApp {
+
+public:
+void init();
+void run();
+void display();
+
+private:
+};
+
+#endif //__COLLISIONTEST03_H__
