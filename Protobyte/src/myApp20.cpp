b2f2c86eff5ee7d5e3d491d6447260b430822719
ira <irajgreenberg@gmail.com>
2015-05-20 13:35:49 -0500

    Experimenting with drawing multiple ellipses and rectangles. Cleaned code to minimize unnecessary white space.

--- Protobyte/src/myApp20.cpp
+++ Protobyte/src/myApp20.cpp
@@ -9,18 +9,11 @@ void myApp20::run() {
 }
 
 void myApp20::display() {
-	//RECT
-		//rect(float radius1, float radius2)
-			//rect(width, height);
-				//Draws rectangle centered at origin (in center of window)
-				//rectangle's width and height are equal to parameters passed to it
-	
-	rect(Vec2(100, 200), Vec2(300, 300));
-
-
-
-	ellipse(100, 200, 10, 10);
-	ellipse(300, 100, 10, 10);
-	//ellipse(200, 0, 10, 10);
-
+	rect(100, 100, 50, 50);
+	rect(200, -50, 50, 34.5);
+	rect(0, 0, 100, 100, CENTER);
+	ellipse(-100, -200, 100, 100);
+	ellipse(-75, -300, 10, 10);
+	ellipse(0, 0, 10, 10);
+	ellipse(200, 0, 10, 10);
 }
