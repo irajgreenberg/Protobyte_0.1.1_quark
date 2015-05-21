1fe8e0adf226ff2f12b02f390aa9558c4e2adac5
ira <irajgreenberg@gmail.com>
2015-05-21 13:19:00 -0500

    Edited myApp20.cpp and myApp20.h. Experimenting with fill, stroke, global variables, and movement.

--- Protobyte/src/myApp20.cpp
+++ Protobyte/src/myApp20.cpp
@@ -2,18 +2,65 @@
 #include "myApp20.h"
 
 void myApp20::init() {
-	background(0.5);
+	setBackground(0.7);
+	std::cout << "width = " << width << std::endl;
+	std::cout << "height = " << height << std::endl;
+
+	std::cout << "width2 = " << getWidth() << std::endl;
+	std::cout << "height2 = " << getHeight() << std::endl;
+
+	std::cout << "canvas size = " << getSize() << std::endl;
+
+	std::cout << appTitle << "c" << std::endl;
+
+	std::cout << "canvasWidth = " << canvasWidth << std::endl;
+	std::cout << "canvasHeight = " << canvasHeight << std::endl;
+
+	std::cout << "windowFrameSize = " << windowFrameSize << std::endl;
+
+	x = -width / 2;
+	y = 0;
 }
 
 void myApp20::run() {
 }
 
 void myApp20::display() {
+	strokeColor = Col4(1);
+	fillColor = red;
+	isStroke = true;
+	lineWidth = 2;
 	rect(100, 100, 50, 50);
 	rect(200, -50, 50, 34.5);
+	isStroke = true;
+	isFill = false;
+	lineWidth = 3;
 	rect(0, 0, 100, 100, CENTER);
 	ellipse(-100, -200, 100, 100);
+	lineWidth = 1;
 	ellipse(-75, -300, 10, 10);
+	isFill = true;
+	fillColor = blue;
 	ellipse(0, 0, 10, 10);
+	fillColor = Col4(0, 1, 1, 1);
 	ellipse(200, 0, 10, 10);
+	ellipse(0, -385, 10, 10);
+
+	fill(green);
+
+	ellipse(x, y, 20, 20);
+	
+	if (x <= -(width / 2)) {
+		xSpeed = 1;
+	}
+	else if (x >= (width / 2)) {
+		xSpeed = -1;
+	}
+	x += xSpeed;
+}
+
+void myApp20::mousePressed() {
+	std::cout << "mouseX = " << mouseX << std::endl;
+	std::cout << "mouseY = " << mouseY << std::endl;
+	std::cout << std::endl;
 }
--- Protobyte/src/myApp20.h
+++ Protobyte/src/myApp20.h
@@ -9,12 +9,33 @@ using namespace ijg;
 
 class myApp20 : public ProtoBaseApp {
 
-public:
-void init();
-void run();
-void display();
+	public:
+		//Variables
+		float minX = -690;
+		float maxX = 690;
+		float minY = -385;
+		float maxY = 385;
 
-private:
+		Col4f red = Col4f(1.0, 0, 0, 1.0);
+
+		Col4 blue = Col4(0, 0, 1.0, 1.0);
+
+		Col4 green = Col4(0.0, 1.0, 0.0, 1);
+
+		Vec2f mine = Vec2(0.5, 0.6);
+
+		int x;
+		int y;
+		
+		int xSpeed = 1;
+
+		//Functions
+		void init();
+		void run();
+		void display();
+		void mousePressed();
+
+	private:
 
 };
 
